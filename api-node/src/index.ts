4dbbb7eb69637b4e6bedbdc590277e2d1f682c17
Raione <raione_bonfim@hotmail.com>
2022-05-20 00:53:42 -0300

    create endpoints

---
+++ api-node/src/index.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response, NextFunction } from 'express';
+import statusRoute from './routes/status.route';
+import usersRoute from './routes/users.route';
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(usersRoute);
+app.use(statusRoute);
+
+app.listen(3333, () => {
+  console.log('Aplicação executando na porta 3333!')
+})
