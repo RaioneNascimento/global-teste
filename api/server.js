5700ce5ffd760c83e3c426713113fa154a95e61c
Raione <raione_bonfim@hotmail.com>
2022-05-25 18:23:05 -0300

    repo front

--- api/server.js
+++
@@ -1,8 +0,0 @@
-const app = require('./src/config/express');
-
-const hostname = 'localhost';
-const port = 8000;
-
-app.listen(port, hostname, () => {
-	console.log(`Server running at http://${hostname}:${port}/`);
-});
